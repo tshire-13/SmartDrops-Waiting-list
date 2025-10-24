52994061c6449bb5e09a9a6fbde651a605cbaf63
Tshireletso Selemela <159533600+tshire-13@users.noreply.github.com>
2025-10-24 13:33:45 +0200

    Updates

---
+++ Backend/index.js
@@ -0,0 +1,16 @@
+import express from "express";
+const app = express();
+import dotenv from 'dotenv'
+
+
+dotenv.config();
+const port = process.env.PORT || 3000
+
+app.get("/", (req, res) => {
+  res.send("Hello from Node.js + Express!");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`)
+});
+
